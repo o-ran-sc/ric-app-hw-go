441de0fa6d046febfc01d51fcf375eaf41fb6834
subhash kumar singh <subh.singh@samsung.com>
2021-10-07 18:07:14 +0530

    Update license header
    Update license header text to fix the license.
    
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>
    Change-Id: I739731b5ef43ccc2ea753eb79d285860f49a4849

--- hwApp.go
+++ hwApp.go
@@ -14,8 +14,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-   This source code is part of the near-RT RIC (RAN Intelligent Controller)
-   platform project (RICP).
 ==================================================================================
 */
 package main
